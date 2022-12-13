a28013828adc3acd6cb66f7be92c4026e0b4df11
Sai Krishna <118363434+iam-krish025@users.noreply.github.com>
2022-12-13 10:36:11 +0530

    Add files via upload

---
+++ Reg/js/add.js
@@ -0,0 +1,8 @@
+function substraction(){
+    let x;
+    x=document.getElementById('xvalue').value;
+    let y;
+    y=document.getElementById('yvalue').value;
+    let z=x-y;
+    document.getElementById('sub').innerHTML=z;
+}
